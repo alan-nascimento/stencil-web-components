aff7031febc157587efeeb74b9d8e3d0a8541d17
Alan Nascimento <alan.silva.n@hotmail.com>
2020-10-30 13:04:14 -0300

    refactor: remove unused components

--- src/components/app-root/app-root.tsx
+++ src/components/app-root/app-root.tsx
@@ -2,7 +2,6 @@ import { Component, h } from '@stencil/core';
 
 @Component({
   tag: 'app-root',
-  styleUrl: 'app-root.css',
   shadow: true,
 })
 export class AppRoot {
@@ -10,16 +9,11 @@ export class AppRoot {
     return (
       <div>
         <header>
-          <h1>Stencil App Starter</h1>
+          <h1>TODO List</h1>
         </header>
 
         <main>
-          <stencil-router>
-            <stencil-route-switch scrollTopOffset={0}>
-              <stencil-route url="/" component="app-home" exact={true} />
-              <stencil-route url="/profile/:name" component="app-profile" />
-            </stencil-route-switch>
-          </stencil-router>
+
         </main>
       </div>
     );
